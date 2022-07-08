c5b1815a42333f6431200911f0520be840605775
Robert Hollingworth <69080884+robbobby@users.noreply.github.com>
2022-07-08 23:22:26 +0100

    update on formatting

--- Api/Repository/UserRepository.cs
+++ Api/Repository/UserRepository.cs
@@ -13,13 +13,7 @@ public class UserRepository : RepositoryBase<User>, IUserRepository {
     }
 
     public async Task<User?> GetUserByEmail(string loginModelEmail) {
-        // get User by email with AccountUser[] where user.EmailAddress == loginModelEmail
         var user = await _dbContext.Users.Include(u => u.AccountUsers).FirstOrDefaultAsync(u => u.EmailAddress == loginModelEmail);
-        // var accountUser = await _dbContext.AccountUsers.FirstOrDefaultAsync(au => au.UserId == user.Id && au.AccountId == user.DefaultAccount);
-        // if (accountUser != null && user != null)
-            // user.AccountUsers = new List<AccountUser>() {
-                // accountUser
-            // };
         return user;
     }
 }
